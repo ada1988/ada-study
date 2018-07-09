550f0009553aaaa22039a507b2fea856953d6a05
ada1988 <1046562032@qq.com>
2018-07-09 22:13:28 +0800

    websocket

---
+++ ada-study-websocket/src/main/java/org/ada/study/websocket/config/WebSocketConfig.java
@@ -0,0 +1,34 @@
+package org.ada.study.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+/**  
+ * Filename: WebSocketConfig.java  <br>
+ *
+ * Description:   <br>
+ * 
+ * @author: CZD <br> 
+ * @version: 1.0 <br> 
+ * @Createtime: 2018年7月9日 <br>
+ *  
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/my-websocket").withSockJS();
+    }
+
+}
